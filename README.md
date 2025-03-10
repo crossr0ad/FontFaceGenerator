713fdfeb54eed47c5ca256624c219c17869ad855
crossr0ad <gran5acance@gmail.com>
2025-03-10 23:25:21 +0900

    feat: add sources and output file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FontFaceGenerator
+
 Generate a CSS file to replace unattractive fonts with your desired ones
+
+## Usage
+
+```bash
+deno run -A main.ts [InputConfig] [OutputCssPath]
+```
+
+The output .css consists of [@font-face](https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face) rules that replace unattractive fonts with your desired ones.
+You can apply this to all pages by browser extension like [Stylus](https://github.com/openstyles/stylus).
---
+++ types.ts
@@ -0,0 +1,21 @@
+import { z } from "https://deno.land/x/zod@v3.24.1/mod.ts";
+
+export const Rule = z.object({
+  description: z.string(),
+  fontFamilies: z.array(z.string()),
+  sources: z.array(z.object({
+    fullNames: z.array(z.string()),
+    weight: z.union([z.number(), z.string()]).optional(),
+    style: z.string().optional(),
+    unicode_range: z.string().optional(),
+  })),
+});
+
+export const Config = z.object({ rules: z.array(Rule) });
+export type Config = z.infer<typeof Config>;
+
+export type Rule = Config["rules"][number];
+
+export function assertIsConfig(object: unknown): asserts object is Config {
+  Config.parse(object);
+}
