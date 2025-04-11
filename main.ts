a68ec67017fa255d5da0d0d09c1831e8f94dd83e
crossr0ad <gran5acance@gmail.com>
2025-04-11 19:58:00 +0900

    fix: weightをまとめて出力しないように

--- main.ts
+++ main.ts
@@ -1,61 +1,67 @@
 import { parse } from "jsr:@std/yaml";
-import { assertIsConfig, Rule } from "./types.ts";
+import { type Rule, assertIsConfig } from "./types.ts";
 
 const main = async (configPath: string, outfile: string): Promise<void> => {
-  const configText = await Deno.readTextFile(configPath);
-  const config = parse(configText);
-  assertIsConfig(config);
-  console.log(`parsed ${config.rules.length} rules from ${configPath}`);
-  console.log();
+	const configText = await Deno.readTextFile(configPath);
+	const config = parse(configText);
+	assertIsConfig(config);
+	console.log(`parsed ${config.rules.length} rules from ${configPath}`);
+	console.log();
 
-  const arr = config.rules.flatMap(procRule);
+	const arr = config.rules.flatMap(procRule);
 
-  await Deno.writeTextFile(outfile, arr.join("\n"));
-  console.log(`wrote ${arr.length} rules to ${outfile}`);
+	await Deno.writeTextFile(outfile, arr.join("\n"));
+	console.log(`wrote ${arr.length} rules to ${outfile}`);
 };
 
 const procRule = (rule: Rule): string[] => {
-  console.log(`processing ${rule.description}...`);
+	console.log(`processing ${rule.description}...`);
 
-  const arr = rule.fontFamilies.flatMap((family) =>
-    rule.sources.flatMap((src) => createFontFace(family, src))
-  );
+	const arr = rule.fontFamilies.flatMap((family) =>
+		rule.sources.flatMap((src) => {
+			if (src.weights) {
+				return src.weights.map((weight) => createFontFace(family, src, weight));
+			}
+			return createFontFace(family, src);
+		}),
+	);
 
-  console.log(`generated ${arr.length} rules!`);
-  console.log();
-  return arr;
+	console.log(`generated ${arr.length} rules!`);
+	console.log();
+	return arr;
 };
 
 const createFontFace = (
-  fontFamily: string,
-  source: Rule["sources"][number],
+	fontFamily: string,
+	source: Omit<Rule["sources"][number], "weights">,
+	weight?: number | string,
 ): string => {
-  const arr: string[] = [];
+	const arr: string[] = [];
 
-  arr.push(`@font-face {`);
-  arr.push(`    font-family: \"${fontFamily}\";`);
+	arr.push("@font-face {");
+	arr.push(`    font-family: \"${fontFamily}\";`);
 
-  if (source.weight) {
-    arr.push(`    font-weight: ${source.weight};`);
-  }
-  if (source.style) {
-    arr.push(`    font-style: ${source.style};`);
-  }
-  if (source.unicode_range) {
-    arr.push(`    unicode-range: ${source.unicode_range};`);
-  }
+	if (weight) {
+		arr.push(`    font-weight: ${weight};`);
+	}
+	if (source.style) {
+		arr.push(`    font-style: ${source.style};`);
+	}
+	if (source.unicode_range) {
+		arr.push(`    unicode-range: ${source.unicode_range};`);
+	}
 
-  const src = source.fullNames.map((name) => `local(\"${name}\")`).join(", ");
-  arr.push(`    src: ${src};`);
+	const src = source.fullNames.map((name) => `local(\"${name}\")`).join(", ");
+	arr.push(`    src: ${src};`);
 
-  arr.push(`}`);
+	arr.push("}");
 
-  return arr.join("\n") + "\n";
+	return `${arr.join("\n")}\n`;
 };
 
 if (import.meta.main) {
-  main(
-    Deno.args[0] || "config/config.yml",
-    Deno.args[1] || "output/output.css",
-  );
+	main(
+		Deno.args[0] || "config/config.yml",
+		Deno.args[1] || "output/output.css",
+	);
 }
--- types.ts
+++ types.ts
@@ -1,14 +1,16 @@
 import { z } from "https://deno.land/x/zod@v3.24.1/mod.ts";
 
 export const Rule = z.object({
-  description: z.string(),
-  fontFamilies: z.array(z.string()),
-  sources: z.array(z.object({
-    fullNames: z.array(z.string()),
-    weight: z.union([z.number(), z.string()]).optional(),
-    style: z.string().optional(),
-    unicode_range: z.string().optional(),
-  })),
+	description: z.string(),
+	fontFamilies: z.array(z.string()),
+	sources: z.array(
+		z.object({
+			fullNames: z.array(z.string()),
+			weights: z.array(z.union([z.number(), z.string()])).optional(),
+			style: z.string().optional(),
+			unicode_range: z.string().optional(),
+		}),
+	),
 });
 
 export const Config = z.object({ rules: z.array(Rule) });
@@ -17,5 +19,5 @@ export type Config = z.infer<typeof Config>;
 export type Rule = Config["rules"][number];
 
 export function assertIsConfig(object: unknown): asserts object is Config {
-  Config.parse(object);
+	Config.parse(object);
 }
